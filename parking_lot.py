ac16b6ab180fc5c3b15b0ffb363992cce7f82507
Claudiu Milea <claudius.milea@gmail.com>
2022-06-27 18:07:39 +0300

    raise exception location already allocated

--- parking_lot.py
+++ parking_lot.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 from typing import Optional, Union
 from enum import Enum
-import uuid
+from uuid import uuid4, UUID
 from pydantic import BaseModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from starlette.responses import JSONResponse
+
 
 app = FastAPI()
 
@@ -12,6 +15,7 @@ PARKED_CARS = {
     '27275d49-7556-448f-b0bc-9bb7770aa9f7': {'tariff': 'hourly', 'location': 7, 'start': '2022-06-25T17:21:33.913233', 'end': None, 'fee': None},
     '730d97b3-dfc7-4205-acee-811f57265bf1': {'tariff': 'daily', 'location': 14, 'start': '2022-06-25T17:23:24.428268', 'end': None, 'fee': None}
 }
+PARKING_LOT_CAPACITY = 14
 
 
 class TariffName(str, Enum):
@@ -19,14 +23,35 @@ class TariffName(str, Enum):
     daily = "daily"
 
 
+class Tariffs(str, Enum):
+    hourly = 1
+    daily = 8
+
+class InvalidLocationException(Exception):
+    def __init__(self, location):
+        self.location = location
+
+class LocationNotAvailableException(Exception):
+    def __init__(self, location):
+        self.location = location
+
+
 class ParkedCar(BaseModel):
-    car_id: str
+    car_id: UUID
     status: str
     tariff: str
     location: int
     start: datetime
     end: datetime | None = None
     fee: float | None = None
+
+@app.exception_handler(InvalidLocationException)
+async def invalid_location_exception_handler(request: Request, exception: InvalidLocationException):
+    return JSONResponse(status_code=422, content={"message": f"Cannot park your car there because location {exception.location} is invalid!"})
+
+@app.exception_handler(LocationNotAvailableException)
+async def location_not_available_exception_handler(request: Request, exception: LocationNotAvailableException):
+    return JSONResponse(status_code=422, content={"message": f"Cannot park your car there because location {exception.location} is not available!"})
 
 
 @app.get("/cars/")
@@ -41,7 +66,14 @@ async def get_car(car_id: Optional[str] = None):
 
 @app.post("/cars/")
 async def add_car(parked_car: ParkedCar):
-    car_id = uuid.uuid4()
+    if parked_car.location <= 0 or parked_car.location > PARKING_LOT_CAPACITY:
+        raise InvalidLocationException(location=parked_car.location)
+
+    if parked_car.location in [x.get('location') for x in PARKED_CARS.values()]:
+        raise LocationNotAvailableException(location=parked_car.location)
+
+
+    car_id = uuid4()
     PARKED_CARS[car_id] = {'status': 'success', 'tariff': parked_car.tariff, 'location': parked_car.location, 'start': parked_car.start, 'end': None, 'fee': None}
     return PARKED_CARS[car_id]
 
